bc383f5d5539df7c03ff2bbe7d26c504c94b7247
Katherine Bui <kaatherine.buii@gmail.com>
2016-11-07 11:34:49 -0800

    change gem file

--- Gemfile
+++ Gemfile
@@ -20,6 +20,8 @@ gem 'awesome_print'
 
 gem 'faker'
 
+gem 'resque-scheduler'
+
 gem 'twilio-ruby', '~> 4.11.1'
 
 group :test do
--- config/environment.rb
+++ config/environment.rb
@@ -15,6 +15,8 @@ require 'pg'
 require 'active_record'
 require 'logger'
 
+require 'resque/scheduler/tasks'
+
 require 'sinatra'
 require "sinatra/reloader" if development?
 
